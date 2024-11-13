445116414a5f98411981730301c52fa12c1df309
behzad aslani <behzadaslani80@gmail.com>
2024-11-13 16:25:21 +0330

    feat: implemented navigation structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-  return <div>World Wise</div>
-}
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Login from './pages/Login'
+import Pricing from './pages/Pricing'
+import Product from './pages/Product'
+import AppLayout from './pages/AppLayout'
+import PageNotFound from './pages/PageNotFound'
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
+        <Route path='app' element={<AppLayout />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 export default App
