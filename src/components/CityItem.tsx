321b14c77294fecdf449880389c3af51840016bc
behzad aslani <behzadaslani80@gmail.com>
2024-11-15 15:12:51 +0330

    feat: implemented storing data in search params

--- src/components/CityItem.tsx
+++ src/components/CityItem.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { CityType } from '../../types'
 import styles from './CityItem.module.css'
 
@@ -13,13 +14,20 @@ const formatDate = (date: Date) =>
     weekday: 'long',
   }).format(new Date(date))
 
-export default function CityItem({ city: { cityName, emoji, date } }: Props) {
+export default function CityItem({
+  city: { cityName, emoji, date, id, position },
+}: Props) {
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>({formatDate(date)})</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   )
 }
