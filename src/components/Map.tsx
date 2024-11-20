e69d9324fa5b634a4a766639f11686867772a3a7
behzad aslani <behzadaslani80@gmail.com>
2024-11-20 13:15:56 +0330

    feat: [wip] implementing map feature

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,18 +1,44 @@
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import styles from './Map.module.css'
+import { useState } from 'react'
+import { LatLngExpression } from 'leaflet'
+import { useCities } from '../contexts/CitiesContexts'
 
 export default function Map() {
-  const navigate = useNavigate()
-  const [searchParams] = useSearchParams()
-  const lat = searchParams.get('lat')
-  const lng = searchParams.get('lng')
+  // const navigate = useNavigate()
+  // const [searchParams] = useSearchParams()
+  // const lat = searchParams.get('lat')
+  // const lng = searchParams.get('lng')
+  const { cities } = useCities()
+
+  const [position, setPosition] = useState<LatLngExpression>([40, 0])
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={position}
+        zoom={10}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        {cities.map((city) => {
+          return (
+            <Marker
+              key={city.id}
+              position={[city.position.lat, city.position.lng]}
+            >
+              <Popup>
+                <span>{city.emoji}</span>
+                <span>{city.cityName}</span>
+              </Popup>
+            </Marker>
+          )
+        })}
+      </MapContainer>
     </div>
   )
 }
